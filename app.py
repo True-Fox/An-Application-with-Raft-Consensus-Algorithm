79331b6abe2aece4043343598b98211f0d1a65bd
True-Fox <pes1202100682@pesu.pes.edu>
2024-04-09 20:14:35 +0530

    added login/signup page

--- app.py
+++ app.py
@@ -1,29 +1,77 @@
-from sqlite3 import Cursor
-from flask import Flask, render_template
-import mysql.connector
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re, random
 
-connection = mysql.connector.connect(
-    host="localhost",
-    user="ambhi",
-    password="ambhi",
-    database="task_mgmt"
-)
+
 app = Flask(__name__)
+app.secret_key = "s3cr34_14_5335MS"
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'ambhi'
+app.config['MYSQL_PASSWORD'] = 'ambhi'
+app.config['MYSQL_DB'] = 'task_mgmt'
 
-cursor = connection.cursor()
+mysql = MySQL(app=app)
 
-if(cursor):
-    print("Connection successful")
 
-@app.route('/')
-def hello():
-    return "Hello, world"
+@app.route('/login/', methods=['GET','POST'])
+def login():
+    msg=''
+    if request.method=='POST':
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("SELECT * FROM users WHERE username=%s AND password=%s",(username,password))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id']=account['user_id']
+            session['username']=account['username']
+            return render_template('index.html',username=session['username'])
+        else:
+            msg = 'Incorrect username/password'
+            print(msg)
+    return render_template('login.html', msg='')
+
+@app.route('/register/',  methods=['GET','POST'])
+def register():
+    msg=''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("SELECT * FROM users WHERE username=%s",(username,))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            id = random.randint(1, 100)
+            role = "user"
+            cursor.execute('INSERT INTO users VALUES (%s, %s, %s, %s, %s)', (id,username, password, email, role))
+            mysql.connection.commit()
+            return render_template("login.html")
+    return render_template('register.html', msg='')
+
+@app.route('/login/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 @app.route('/home')
 def home():
-    cursor.execute("SELECT * FROM Tasks")
-    rows = cursor.fetchall()
-    return render_template("index.html", tasks= rows)
+    if session['username']:
+        return render_template("index.html", username=session['username'])
+    else:
+        return render_template("login.html")
 
 
 if __name__ == "__main__":
